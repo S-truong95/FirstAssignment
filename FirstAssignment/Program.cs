26f9ae09c401bf688669afe42b8f1dfad5d7bd7d
Steven <steventruong1995@yahoo.com>
2019-09-04 16:06:20 -0400

    sum and difference

--- FirstAssignment/Program.cs
+++ FirstAssignment/Program.cs
@@ -7,11 +7,12 @@ namespace FirstAssignment
         static void Main(string[] args)
         {
             int bobAge = 10;
-            int annAge = 15;
+            int sueAge = 15;
 
             Console.WriteLine("bob and ann are siblings");
             Console.WriteLine("bob is" + " " + bobAge);
-            Console.WriteLine("ann is" + " " annAge);
+            Console.WriteLine("ann is" + " " + sueAge);
+            Console.WriteLine("the sum of their ages is" + " " + (bobAge+sueAge));
 
         }
     }
